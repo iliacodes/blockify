5f67b0146fe293b43d032c15b0487dc37dc99a8a
iliacodes <6ac177ed@protonmail.com>
2023-03-02 14:58:45 -0800

    finilized form submission and created new contract for deployment. Allow site to have display previous interactions with contract.

--- contracts/BlockifyPortal.sol
+++ contracts/BlockifyPortal.sol
@@ -3,18 +3,62 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract BlockifyPortal {
-  uint totalRecommendations;
+  uint256 totalRecommendations;
+  uint private seed;
+
+  event NewRecommendation(address indexed from, uint256 timestamp, string message);
+
+  struct Recommendation {
+    address recommender; // address of recommender
+    string message; //message of recommender
+    uint256 timestamp; // timestamp of recommendation
+  }
+
+  mapping(address => uint256) public lastRecommendedAt;
+
+  Recommendation[] recommendations;
 
     constructor() {
         console.log("Yo yo, I am the smartest contract.");
-    }
-    function recommend() public {
-      totalRecommendations += 1;
-      console.log("%s has recommended Blockify!", msg.sender);
+
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
-    function getTotalRecommendations() public view returns (uint) {
-      console.log("We have %d total recommendations!", totalRecommendations);
+    function recommend(string memory _message) public {
+
+      require (
+        lastRecommendedAt[msg.sender] + 15 minutes < block.timestamp,
+        "Wait 15m"
+      );
+
+      lastRecommendedAt[msg.sender] = block.timestamp;
+      totalRecommendations += 1;
+      console.log("%s recommended w/ message %s!", msg.sender, _message);
+
+      recommendations.push(Recommendation(msg.sender, _message, block.timestamp));
+
+      seed = (block.difficulty + block.timestamp + seed) % 100;
+
+      if (seed <= 50) {
+        console.log("%s won!", msg.sender);
+
+        uint prizeAmount = 0.00069 ether;
+        require(
+          prizeAmount <= address(this).balance,
+          "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+      }
+
+      emit NewRecommendation(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllRecommendations() public view returns (Recommendation[] memory) {
+      return recommendations;
+    }
+
+    function getTotalRecommendations() public view returns (uint256) {
       return totalRecommendations;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,34 +1,51 @@
 const main = async() => {
-  const owners = []; // declare empty array
+  // const owners = []; // declare empty array
  
-  console.log("Owners:", owners); // print the array of owners
+  // console.log("Owners:", owners); // print the array of owners
 
-  const [owner, randomPerson] = await hre.ethers.getSigners();
-  owners.push(owner.address); // push owner address into array
-  owners.push(randomPerson.address); // push randomPerson address into array
+  // const [owner, randomPerson] = await hre.ethers.getSigners();
+  // owners.push(owner.address); // push owner address into array
+  // owners.push(randomPerson.address); // push randomPerson address into array
 
   const blockifyContractFactory = await hre.ethers.getContractFactory('BlockifyPortal');
   const blockifyContract = await blockifyContractFactory.deploy();
   await blockifyContract.deployed();
+  console.log("Addy of contract: ", blockifyContract.address)
 
-  console.log("Contract deployed to:", blockifyContract.address);
-  console.log("Contract deployed by:", owner.address);
+  // console.log("Contract deployed to:", blockifyContract.address);
+  // console.log("Contract deployed by:", owner.address);
+
+  let blockifyCount;
+  blockifyCount = await blockifyContract.getTotalRecommendations();
+  console.log("Blockify count:", blockifyCount.toNumber());
 
 
-  await blockifyContract.getTotalRecommendations();
+
+  let blockifyTxn = await blockifyContract.recommend("A message is here!");
+  await blockifyTxn.wait(); // wait until the transaction is mined
+
+  const [_, newPerson] = await hre.ethers.getSigners();
+  blockifyTxn = await blockifyContract.connect(newPerson).recommend("Sending another message...")
+  await blockifyTxn.wait(); // wait until the second transaction is mined
+
+  let allRecommendations = await blockifyContract.getAllRecommendations();
+  console.log("All recommendations:", allRecommendations)
+
+
+  // await blockifyContract.getTotalRecommendations();
 
   //first check
-  const firstBlockifyTxn = await blockifyContract.recommend();
-  await firstBlockifyTxn.wait();
+  // const firstBlockifyTxn = await blockifyContract.recommend();
+  // await firstBlockifyTxn.wait();
 
   //second check
-  const secondBlockifyTxn = await blockifyContract.connect(randomPerson).recommend();
-  await secondBlockifyTxn.wait();
+  // const secondBlockifyTxn = await blockifyContract.connect(randomPerson).recommend();
+  // await secondBlockifyTxn.wait();
 
   // = await 
-  await blockifyContract.getTotalRecommendations();
+  // await blockifyContract.getTotalRecommendations();
   
-  console.log("Owners:", owners); // print the array of owners for total results
+  // console.log("Owners:", owners); // print the array of owners for total results
 }
 
 const runMain = async () => {
